2a6a6cf8e95ecebc375c1fb557edeba780782ec3
DavidDev25 <david.werner@david-werner.dev>
2025-03-28 19:45:29 +0100

    feat(landingpage): add landing page component with sections for home, about, skills, projects, and contact

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from "../pages/home/home.component";
+import { ProjectsComponent } from "../pages/projects/projects.component";
+import { AboutMeComponent } from "../pages/about-me/about-me.component";
+import { SkillsComponent } from "../pages/skills/skills.component";
+import { ContactMeComponent } from "../pages/contact-me/contact-me.component";
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [HomeComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, ContactMeComponent],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+
+}
